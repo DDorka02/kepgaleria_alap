338b03d2f4cc09b3ad3df96283867b2e96dfc720
DDorka02 <dobszaydorka@gmail.com>
2024-04-02 16:22:48 +0200

    ll

--- main.js
+++ main.js
@@ -3,38 +3,38 @@ import { htmlOsszeAllit } from "./fuggvenyek.js";
 
 /*1. feladat:  galéria kisképeinek elhelyezése */
 /* fogd meg a .galeria elemet */
-    const galeriaELEMEK = document.getElementsByClassName("galeria")[0]
+    const galeriaELEMEK = galeriaELEMEK.eq(0)
     console.log(galeriaELEMEK)
 /* írd bele az összeálíltott html képsorozatot */
-    galeriaELEMEK.innerHTML=htmlOsszeAllit(KEPEK)
+    galeriaELEMEK.html=htmlOsszeAllit(KEPEK)
 
 /*2. feladat  A .nagykep divben lévő img elem megfogása */
-    const nagykepIMGELEM = document.querySelector(".nagykep img")
+    const nagykepIMGELEM = $(".nagykep img")
     console.log(nagykepIMGELEM)
 /* A .nagykep div megfogása */
-    const nagykepIMGELEM2 = document.getElementsByClassName("nagykep")[0]
+    const nagykepIMGELEM2 = $(".nagykep")[0]
     console.log(nagykepIMGELEM2)
 /*3. feladat-  eseménykezelő hozzáadása a kisképekhez  */
 /* Az előzőekben létrehozott kisképek megfogása (querySelectorAll) - ez egy lista lesz */
-    const kisKEPELEMEK = document.querySelectorAll(".kep");
+    const kisKEPELEMEK =$(".kep");
 
 /* eseménykezelő hozzáadása a kisképekhez, ciklussal */
     for (let index = 0; index < kisKEPELEMEK.length; index++) {
-        kisKEPELEMEK[index].addEventListener("click",function (event){
+        kisKEPELEMEK[index].on("click",function (event){
             console.log(event.target.src)
             aktIndex=index;
-            nagykepIMGELEM.src=event.target.src
-            nagykepIMGELEM2.classList.remove("rejtett")
+            nagykepIMGELEM.attr=event.target.src
+            nagykepIMGELEM2.removeClass("rejtett")
         });
     
     }
 
 /* Az eseménykezelő függvényében: ha a kisképre kattintunk, akkor a nagykép src attribútumát le kell cserélni a kiskép src attribútumában megadott elérési útvonalra  */
     let aktIndex=0;
-    const jobbgomb = document.querySelector(".jobb")
-    jobbgomb.addEventListener("click",jobbra)
-    const balgomb = document.querySelector(".bal")
-    balgomb.addEventListener("click", balra)
+    const jobbgomb = $(".jobb")
+    jobbgomb.on("click",jobbra)
+    const balgomb = $(".bal")
+    balgomb.on("click", balra)
 
 
 /* 4. feladat .bal és a .jobb gombokra kattintva léptessük a galériát */
@@ -45,7 +45,7 @@ import { htmlOsszeAllit } from "./fuggvenyek.js";
         }
         console.log(aktIndex)
         console.log(KEPEK[aktIndex].kep)
-        nagykepIMGELEM.src = KEPEK[aktIndex].kep
+        nagykepIMGELEM.attr = KEPEK[aktIndex].kep
     }
 
     function balra() {
@@ -55,13 +55,13 @@ import { htmlOsszeAllit } from "./fuggvenyek.js";
         }
         console.log(aktIndex)
         console.log(KEPEK[aktIndex].kep)
-        nagykepIMGELEM.src = KEPEK[aktIndex].kep
+        nagykepIMGELEM.attr = KEPEK[aktIndex].kep
 
 
     }
 
-    const zarELEM =document.querySelector(".zar button")
-    zarELEM.addEventListener("click", function () {
-    nagykepIMGELEM2.classList.add("rejtett") 
+    const zarELEM =$(".zar button")
+    zarELEM.on("click", function () {
+    nagykepIMGELEM2.addClass("rejtett") 
         
     })
